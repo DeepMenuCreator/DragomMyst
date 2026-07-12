36d0dc3fd664891dbd5f3c017c3e89a04dd4e221
DeepMenuCreator <solvers657@gmail.com>
2026-07-12 07:36:31 +0500

    Delete ChatColorUtil.java

--- ChatColorUtil.java
+++
@@ -1,8 +0,0 @@
-package ru.myst.manager;
-
-public class ChatColorUtil {
-    public static String strip(String input) {
-        if (input == null) return "";
-        return input.replaceAll("(?i)&[0-9A-FK-OR]", "");
-    }
-}
